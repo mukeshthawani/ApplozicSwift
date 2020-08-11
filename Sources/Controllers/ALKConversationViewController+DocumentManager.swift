eeed6da83bbb53cc460ca10695ac5e963879fdf4
mukeshthawani <mukesh9039@gmail.com>
2020-08-11 17:10:24 +0530

    Use NSFileCoordinator for reading docs

--- Sources/Controllers/ALKConversationViewController+DocumentManager.swift
+++ Sources/Controllers/ALKConversationViewController+DocumentManager.swift
@@ -9,25 +9,23 @@ import UIKit
 import Applozic
 
 extension ALKConversationViewController: ALKDocumentManagerDelegate {
-    func documentSelected(at urls: [URL]) {
-        for index in 0 ..< urls.count {
-            let filePath = urls[index]
-            let (message, indexPath) = viewModel.sendFile(
-                at: filePath,
-                metadata: configuration.messageMetadata
-            )
-            guard message != nil, let newIndexPath = indexPath else { return }
-            self.tableView.beginUpdates()
-            self.tableView.insertSections(IndexSet(integer: newIndexPath.section), with: .automatic)
-            self.tableView.endUpdates()
-            self.tableView.scrollToBottom(animated: false)
-            guard let cell = tableView.cellForRow(at: newIndexPath) as? ALKMyDocumentCell else { return }
-            guard ALDataNetworkConnection.checkDataNetworkAvailable() else {
-                let notificationView = ALNotificationView()
-                notificationView.noDataConnectionNotificationView()
-                return
-            }
-            viewModel.uploadImage(view: cell, indexPath: newIndexPath)
+    func documentSelected(at url: URL, fileName: String) {
+        let (message, indexPath) = viewModel.sendFile(
+            at: url,
+            fileName: fileName,
+            metadata: configuration.messageMetadata
+        )
+        guard message != nil, let newIndexPath = indexPath else { return }
+        self.tableView.beginUpdates()
+        self.tableView.insertSections(IndexSet(integer: newIndexPath.section), with: .automatic)
+        self.tableView.endUpdates()
+        self.tableView.scrollToBottom(animated: false)
+        guard let cell = tableView.cellForRow(at: newIndexPath) as? ALKMyDocumentCell else { return }
+        guard ALDataNetworkConnection.checkDataNetworkAvailable() else {
+            let notificationView = ALNotificationView()
+            notificationView.noDataConnectionNotificationView()
+            return
         }
+        viewModel.uploadImage(view: cell, indexPath: newIndexPath)
     }
 }
--- Sources/Utilities/Data+Extension.swift
+++ Sources/Utilities/Data+Extension.swift
@@ -24,16 +24,4 @@ extension Data {
         }
         return nil
     }
-
-    func saveToDisk(fileExtension: String) -> Result<URL, Error> {
-        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        let filePath = String(format: "/%f.%@", Date().timeIntervalSince1970 * 1000, fileExtension)
-        let fullPath = documentsURL.appendingPathComponent(filePath)
-        do {
-            try write(to: fullPath, options: .atomic)
-            return .success(fullPath)
-        } catch let error {
-            return .failure(error)
-        }
-    }
 }
